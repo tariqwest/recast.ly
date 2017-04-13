c441e6d0dc564449faa8442cf9626584989561df
Hack Reactor Student <student@hackreactor.com>
2017-04-12 17:51:54 -0700

    Update to pass videolist, videolistentry, videoplayer tests

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,45 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer/>
-    </div>
-    <div className="col-md-5">
-      <VideoList/>
-    </div>
-  </div>
-);
+// var App = (props) => {
 
-// In the ES6 spec, files are "modules" and do not share a top-level scope
-// `var` declarations will only exist globally where explicitly defined
+// console.log(props)
+
+// return (
+//   <div>
+//     <Nav />
+//     <div className="col-md-7">
+//       <VideoPlayer />
+//     </div>
+//     <div className="col-md-5">
+//       <VideoList videos={exampleVideoData} />
+//     </div>
+//   </div>
+// )};
+
+// // In the ES6 spec, files are "modules" and do not share a top-level scope
+// // `var` declarations will only exist globally where explicitly defined
+// window.App = App;
+
+/**********REFACTOR***********/
+
+var App = class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+
+    return ( 
+      <div>
+        <Nav />
+          <div className="col-md-7">
+          <VideoPlayer video={this.props.videos[0]} />
+      </div>
+      <div className="col-md-5">
+        <VideoList videos={this.props.videos} />
+      </div>
+    </div>
+    );
+  }
+};
+
 window.App = App;
